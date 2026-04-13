911318d6f69fbed7ad60f61e664acfbc5ae23bfa
laksithnimesha <152592308+laksithnimesha@users.noreply.github.com>
2026-04-13 12:47:46 +0530

    building gui

---
+++ gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("CPU Scheduling Analysis Tool")
+root.geometry("800x600")
+
+label = tk.Label(root, text="CPU Scheduling Tool", font=("Arial", 18))
+label.pack(pady=20)
+
+root.mainloop()
